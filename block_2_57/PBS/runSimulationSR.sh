89b2cb1e54a8fdc3385e592e9baf94b0af9a2a64
Yulin Sun <ys7485@gadi-login-07.gadi.nci.org.au>
2025-10-07 12:07:46 +1100

    Initial commit: add all CH2 files

---
+++ block_2_57/PBS/runSimulationSR.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+
+cd $PBS_JOBFS
+SECONDS=0
+
+# Rename the first and second arguments passed to this single shot script for clarity
+MODELINDEX=$1
+SEED=$2
+FILENAME=${MODELINDEX}_${SEED}
+JOBNAME=block_2_57
+FULLJOBNAME=$JOBNAME
+TESTDIR=$HOME/CH2/$JOBNAME
+
+if [ -f $TESTDIR/done/${FILENAME} ]; then
+     echo "$FILENAME already done! Moving to next simulation."
+     exit 0
+fi
+
+# Get the correct model index from the file: put into array
+# From the table with all parameter combination: combo.csv
+MODEL_FILE=$TESTDIR/R/combos.csv
+MODEL_NUM=($(awk "NR==$MODELINDEX" $MODEL_FILE))
+
+# Read in seeds from the list of seeds: seeds.csv
+SEED_FILE=$TESTDIR/R/seeds.csv
+SEED_NUM=($(awk "NR==$SEED" $SEED_FILE))
+
+# Output a message indicating which model and seed are being run
+# Run the model
+echo "Running modelindex = $MODELINDEX, seed = $SEED...\n"
+$HOME/software/slim-install/bin/slim -s ${SEED_NUM} -d modelindex=$MODELINDEX -d m=${MODEL_NUM[0]} -d direction=${MODEL_NUM[1]} -d gf_time_frac=${MODEL_NUM[2]} -d r=${MODEL_NUM[3]} $TESTDIR/slim/script.slim
+
+# Record the elapsed time and output a completion message
+# Convert the seconds into hours, minutes, and seconds for readable outputs
+DURATION=$SECONDS
+echo "Run modelindex = $MODELINDEX, seed = $SEED finished!"
+echo "$(($DURATION / 3600)) hours, $((($DURATION / 60) % 60)) minutes, and $(($DURATION % 60)) seconds elapsed."
+
+# Create an empty file in the done directory with the model/seed name to indicate the specific simulation has been completed
+touch $TESTDIR/done/${FILENAME}
